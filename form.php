1b770da9ac5e9d25b5d09effca954cb474d512c5
Anton Borisov <32795987+boran1999@users.noreply.github.com>
2019-04-10 18:40:09 +0300

    Create form.php

---
+++ form.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="utf-8">
+   <title>Succes!</title>
+  </head>
+  <body bgcolor="lightblue">
+  	<h2><font color="blue">Ваша форма успешна отправлена!</font></h2>
+  	<p><a href="/sait">отправить форму ещё раз!</a></p>
+  </body>
+  </html>
